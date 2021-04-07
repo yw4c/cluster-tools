66c90530e24d345669b1d6104efabb59fd2fe792
ares.chan <ares.chan@hwaotech.com>
2021-04-08 01:45:06 +0800

    add http response

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,8 +14,28 @@ func main() {
 		c.JSON(200, gin.H{
 			"xff":       c.Request.Header.Get("X-Forwarded-For"),
 			"client IP": c.ClientIP(),
+			"egress IP": getEgressIP(),
+			"POD NAME":  os.Getenv("POD_NAME"),
 		})
 	})
 
-	r.Run()
+	r.Run(":8081")
+}
+
+func getEgressIP() string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.myip.com", nil)
+	if err != nil {
+		return ""
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+	if body, err := ioutil.ReadAll(resp.Body); err != nil {
+		return ""
+	} else {
+		return string(body)
+	}
 }
